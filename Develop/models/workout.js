fcd99e3aa6a99c66d7dc11111b49a990b3a584b2
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-01 22:00:21 -0500

    Added models.export to export workout file.

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -43,7 +43,10 @@ const workoutSchema = new Schema({
 // Function call //
   workoutSchema.virtual("totalDuration").get(function() {
     return this.exercises.reduce((total, exercise) => {
-      return total + exercise.duration;
+      return total + exercises.duration;
     }, 0);
   });
   
+  const Workout = mongoose.model("Workout", workoutSchema);
+// Exporting Workout //
+module.exports = Workout;
