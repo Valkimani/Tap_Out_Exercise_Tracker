82b5c63abb58bc965527c1c6e1608f4d5acc8166
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-01 21:35:36 -0500

    Added required fields in the workout.js file.

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,3 +2,29 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const workoutSchema = new Schema({
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Type in preferred exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Type in exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Type in the exercise duration in minutes"
+        },
+       
+      }
+    ]
+  });
+  
