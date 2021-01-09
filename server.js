70ae53d439a257541ff94e5094110b6fa8c830ed
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-09 16:19:02 -0500

    Updated the Api and html routes and linked in the server.js file.

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan");
-const apiRoute = require("./routes/apiRoute.js");
-const htmlRoute = require("./routes/htmlRoute.js");
+const morgan = require("morgan");
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
 
 const app = express();
 // Setting port server//
 const PORT = process.env.PORT || 3001;
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/Tap_out_db",
-    { useNewUrlParser: true,useUnifiedTopology: true }
+    { useNewUrlParser: true,
+        useFindAndModify:false
+        // useUnifiedTopology: true 
+    }
   );
 //   Link to the routes//
-
+app.use(htmlRoutes);
+app.use(apiRoutes);
 app.listen(PORT, () => {
-  console.log("App running on port!"+PORT);
+    console.log("App running on port!"+PORT);
 });
